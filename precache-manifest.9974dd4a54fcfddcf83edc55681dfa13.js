11720084b25129d97ba78c3a3e1e16cba9874b9a
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2019-11-19 07:16:41 -0800

    Updates

---
+++ precache-manifest.9974dd4a54fcfddcf83edc55681dfa13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ad8d380b16ddc44e909b22f334c8ea",
+    "url": "/AvanticaTraining/index.html"
+  },
+  {
+    "revision": "1c585699fcd076077321",
+    "url": "/AvanticaTraining/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "6d01f101a9fe4bb98e23",
+    "url": "/AvanticaTraining/static/css/main.7681292b.chunk.css"
+  },
+  {
+    "revision": "1c585699fcd076077321",
+    "url": "/AvanticaTraining/static/js/2.3f8838d4.chunk.js"
+  },
+  {
+    "revision": "6d01f101a9fe4bb98e23",
+    "url": "/AvanticaTraining/static/js/main.754ab618.chunk.js"
+  },
+  {
+    "revision": "a3901c9f6208d84f0a84",
+    "url": "/AvanticaTraining/static/js/runtime-main.63c43b7f.js"
+  },
+  {
+    "revision": "3d84ed28da5eb57b70a64878979dde42",
+    "url": "/AvanticaTraining/static/media/avantica.3d84ed28.png"
+  }
+]);
